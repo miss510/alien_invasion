7ea5fe4ce261768befb5dc3759394cde503034a4
miss <845001137@qq.com>
2018-10-30 20:25:37 +0800

    添加了新文件

---
+++ alien_invasion.py
@@ -0,0 +1,35 @@
+import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+
+def run_game():
+    #初始化游戏并创建一个屏幕对象
+    pygame.init()#初始化背景设置
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    #创建一艘飞船
+    ship = Ship(ai_settings,screen)
+    #创建一个用于存储子弹的编组
+    bullets = Group()
+    #创建一个外星人
+    alien = Alien(ai_settings, screen)
+    #设置背景色
+    bg_color = (230,230,230)
+
+    #开始游戏的主循环
+    while True:
+
+        #监视键盘和鼠标事件
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+
+run_game()
